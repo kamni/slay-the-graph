5cb887161c69ae22444fdba75929030df0b6dae4
J Leadbetter <j@jleadbetter.com>
2024-10-22 18:21:24 +0200

    Implement default of 10 levels

--- slay_the_graph/__main__.py
+++ slay_the_graph/__main__.py
@@ -8,6 +8,5 @@ from .view import display_graph
 
 
 if __name__ == '__main__':
-    graph = generate_graph(2)
+    graph = generate_graph(7)
     display_graph(graph)
-    import pdb; pdb.set_trace()
